57752169bdf77ef310755d38e1444abb7bee470a
Rodrigo Roma <rmarsan@estudio-marsan.com.ar>
2021-01-25 19:56:16 -0300

    Estructura Express y Home

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require ('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname,"./public")))
+app.listen(4000, () => console.log('Servidor levantado en el puerto 4000'));
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname,"./views/index.html")));
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")));
+
