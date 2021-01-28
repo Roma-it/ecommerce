cf3bf10aa660bdc8773e244209bd514bddb767e6
polaquim83 <martingol@gmail.com>
2021-01-28 02:41:00 -0300

    view productCart

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(express.static(path.resolve(__dirname,"./public")))
 app.listen(4000, () => console.log('Servidor levantado en el puerto 4000'));
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname,"./views/index.html")));
 app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")));
-app.get('/product', (req,res) => res.sendFile(path.resolve(__dirname,"./views/productDetail.html")));
+app.get('/productDetail', (req,res) => res.sendFile(path.resolve(__dirname,"./views/productDetail.html")));
+app.get('/productCart', (req,res) => res.sendFile(path.resolve(__dirname,"./views/productCart.html")));
 
 
